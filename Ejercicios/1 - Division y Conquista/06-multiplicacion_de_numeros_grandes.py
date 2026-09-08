26a4808f21fe42ab16e6c7a5e363e2867e5c2775
mirkopresa <mirkocamilopresa@proton.me>
2026-09-08 22:04:47 +0000

    Ejercicio 6 resuelto de la guia de Division y Conquista

---
+++ Ejercicios/1 - Division y Conquista/06-multiplicacion_de_numeros_grandes.py
@@ -0,0 +1,32 @@
+# Implementar un algoritmo de multiplicación de dos números grandes de longitud n, por división y conquista,
+# con un orden de complejidad mejor que O(n^2)
+# Justificar la complejidad del algoritmo mediante el teorema maestro.
+
+
+def multiplicar(a, b):
+    n = obtener_longitud(a)
+    if n < 50:
+        return a * b
+    a1, a0 = separar(a)
+    b1, b0 = separar(b)
+    p = multiplicar(a1 + a0, b1 + b0)
+    a0b0 = multiplicar(a0, b0)
+    a1b1 = multiplicar(a1, b1)
+    return a1b1 * 10**n + (p - a0b0 - a1b1) * 10 ** (n // 2) + a0b0
+
+
+def separar(n) -> tuple:
+    n1, n0 = n, 0
+    potencia = 1
+    for _ in range(obtener_longitud(n) // 2):
+        n0 += (n1 % 10) * potencia
+        n1 //= 10
+        potencia *= 10
+    return n1, n0
+
+
+def obtener_longitud(n) -> int:
+    return len(str(n))
+
+
+print(multiplicar(1000, 1000))
